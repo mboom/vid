ae70b8a47eb00a1ac1a6b1b5252ead4ab2f35ca3
Marco Boom <m.boom@student.tudelft.nl>
2018-01-14 15:52:36 +0100

    changed file extension from html to xhtml

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join('index.html'));
+  res.sendFile(path.join('index.xhtml'));
 });
 
 app.listen(PORT, HOST);
