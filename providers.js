a50d272432430c16ce30dfd51f6ab6847fd709bb
Marco Boom <m.boom@student.tudelft.nl>
2018-01-15 11:49:17 +0100

    fixed checking bug

--- providers.js
+++ providers.js
@@ -114,7 +114,7 @@ var providers = {
 
 
     "checkStream": function (id, name) {
-        if (this.checkProvider(id)) {
+        if (!this.checkProvider(id)) {
             return false;
         }
 
@@ -129,7 +129,7 @@ var providers = {
 
 
     "checkSource": function (id, name, protocol) {
-        if (this.checkStream(id, name)) {
+        if (!this.checkStream(id, name)) {
             return false;
         }
 
